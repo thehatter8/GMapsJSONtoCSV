787fd5b06f954ca851e2443e4295e8521e38fe63
Cody <51207609+thehatter8@users.noreply.github.com>
2021-12-05 11:11:38 -0700

    Create takeout.py
    Main project file which opens all files in chosen directory and outputs to csv in chosen directory

---
+++ takeout.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import json
+import csv
+import os
+
+for file in os.listdir('C:\\SOME FOLDER\\Takeout\\Takeout\\Location History\\Semantic Location History\\all'):
+    jsonFile = f"C:\\SOME FOLDER\\Takeout\\Takeout\\Location History\\Semantic Location History\\all\\{file}"
+    with open(jsonFile, 'r') as f:
+        data = json.load(f)
+    features = []
+    for obj in data:
+        for item in enumerate(data[obj]):
+            try:
+                latitudeE7 = int(item[1]['placeVisit']['location']['latitudeE7'])
+                longitudeE7 = int(item[1]['placeVisit']['location']['longitudeE7'])
+                latitude = latitudeE7/10000000
+                longitude = longitudeE7/10000000
+                features.append({'latitude':latitude,'longitude':longitude})
+            except:
+                print('--')
+
+    fields = ['latitude','longitude']
+    with open('C:\\SOME FOLDER\\output.csv', 'a', newline='') as testFile:
+        writer = csv.DictWriter(testFile, fieldnames=fields)
+        writer.writeheader()
+        for loc in features:
+            writer.writerow(loc)
+testFile.close()
