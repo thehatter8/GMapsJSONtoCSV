429430a0bb64f5de7543ac5e20f0d7925dd3bd55
Cody <51207609+thehatter8@users.noreply.github.com>
2022-02-11 01:25:52 -0700

    Reformatting, moved a few lines, added comments

--- takeout.py
+++ takeout.py
@@ -3,31 +3,42 @@ import json
 import csv
 import os
 
+# Mostly for my own curiosity
 start = timeit.default_timer()
-maindir = '{SOMEFOLDER}\\Takeout\\Location History\\Semantic Location History\\'
+
+# Set {SOMEDIR} As shown in README or set to a custom directory
+maindir = '{SOMEDIR}\\Takeout\\Location History\\Semantic Location History\\'
 features = []
+
+# Gets list of years you have data for, and iterates through the months of each year
 for folder in os.listdir(maindir):
     jsonFileList = os.listdir(maindir+folder)
     for jsonFile in jsonFileList:
-        with open(maindir+folder+"\\"+str(jsonFile), 'r', encoding="utf8") as f:
+        currentFile = maindir+folder+"\\"+str(jsonFile)
+        with open(currentFile, 'r', encoding="utf8") as f:
             data = json.load(f)
         for obj in data:
             for item in enumerate(data[obj]):
                 try:
                     latitudeE7 = int(item[1]['placeVisit']['location']['latitudeE7'])
                     longitudeE7 = int(item[1]['placeVisit']['location']['longitudeE7'])
+                    # E7 here is Scientific notation for lat/long * 10^7, so this returns them to GPS coords
                     latitude = latitudeE7/10000000
                     longitude = longitudeE7/10000000
                     features.append({'latitude':latitude,'longitude':longitude})
                 except:
-                    print('--')
-        fields = ['latitude','longitude']
+                    print('Current item is not lat/long')
+
+# Creates a CSV file which allows duplicate coordinates, which come from visiting the same location multiple times
+fields = ['latitude','longitude']
 with open('{SOMEDIR}\\outputWithDupes.csv', 'a', newline='') as testFile:
     writer = csv.DictWriter(testFile, fieldnames=fields)
     writer.writeheader()
     for loc in features:
         writer.writerow(loc)
 testFile.close()
+
+# Checks the previous CSV for duplicates and creates a new CSV without any duplicated coordinate pairs
 with open('{SOMEDIR}\\outputWithDupes.csv', 'r') as in_file, open('{SOMEDIR}\\finalNoDupes.csv','w') as out_file:
     seen = set()
     for line in in_file:
